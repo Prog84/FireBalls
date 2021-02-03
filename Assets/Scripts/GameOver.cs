8dfafef986f69b1c27f1805826c823c7c13be93e
Prog84 <bvv198424@rambler.ru>
2021-02-03 15:50:27 +1000

    Add Bullets, Obstacle, BlockEffect, Water Shader

---
+++ Assets/Scripts/GameOver.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class GameOver : MonoBehaviour
+{
+    [SerializeField] private Tower _tower;
+
+    private void OnEnable()
+    {
+        _tower.GameOver += OnGameOver;
+    }
+    private void OnDisable()
+    {
+        _tower.GameOver -= OnGameOver;
+    }
+
+    private void OnGameOver()
+    {
+        Debug.Log("Башня уничтожена!!!");
+    }
+}
